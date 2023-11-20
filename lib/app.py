4df67d7d6810944aab50bbde3ff53ffa8717c73d
bill <bill.arnold@student.moringaschool.com>
2023-11-20 12:37:32 +0300

    complete

--- lib/app.py
+++ lib/app.py
@@ -3,10 +3,11 @@ print("Hello world!")
 print("Hello sun!")
 print("Hello sky!")
 
-print("Hello world!", end =" ")
+print("Hello world!", end = " ")
 print("Hello sun!", end="!! ")
 print("Hello sky!", end="!!!\n")
 
+path="lib/app.py"
 def test_app_py_exists():
     assert(path.exists("lib/app.py"))
 
